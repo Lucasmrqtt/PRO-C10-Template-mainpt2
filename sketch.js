0402c394ad4cbfa1b2255abb2d6e67364258e31a
Lucasmrqtt <marquettilu@gmail.com>
2022-07-06 20:06:30 -0300

    Projeto 10

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ var seaImg,shipImg;
 function preload(){
   // Descomente o código para adicionar animação ao navio
 
-  shipImg1 = loadAnimation("./images/ship-1.png","./images/ship-2.png");
+  shipImg1 = loadAnimation("./Images/ship-1.png","./Images/ship-2.png");
 
   
-  seaImg = loadImage("./images/sea.png");
+  seaImg = loadImage("./Images/sea.png");
 }
 
 function setup(){
@@ -33,10 +33,10 @@ function draw() {
 
   // Descomente o código para redefinir o fundo
   if(sea.x < 0){
-    sea.x = 0;
-    sea.x = sea.width;
+    //sea.x = 0;
+    //sea.x = sea.width;
     sea.x = sea.width/8;
-    sea.y = height/2;
+    //sea.y = height/2;
   }
 
  
